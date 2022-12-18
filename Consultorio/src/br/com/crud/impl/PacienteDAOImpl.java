a867a2907d349d971247c21b7b31ca97063f08be
diegoreiss <diegoportal.reis@outlook.com>
2022-12-18 17:54:53 -0300

    metodo criar implementado

--- Consultorio/src/br/com/crud/impl/PacienteDAOImpl.java
+++ Consultorio/src/br/com/crud/impl/PacienteDAOImpl.java
@@ -1,14 +1,50 @@
 package br.com.crud.impl;
 
+import br.com.crud.connection.ConnectionManager;
 import br.com.crud.dao.PacienteDAO;
 import br.com.crud.entities.Paciente;
 
+import java.sql.*;
 import java.util.List;
 
 public class PacienteDAOImpl implements PacienteDAO {
     @Override
     public Paciente criar(Paciente paciente) {
-        return null;
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = ConnectionManager.abrirConexao();
+
+            preparedStatement = connection.prepareStatement(
+                    "INSERT INTO paciente " +
+                            "(nome, cpf, nascimento) " +
+                            "VALUES " +
+                            "(?, ?, ?);",
+                    Statement.RETURN_GENERATED_KEYS
+            );
+
+            preparedStatement.setString(1, paciente.getNome());
+            preparedStatement.setString(2, paciente.getCpf());
+            preparedStatement.setDate(3, Date.valueOf(paciente.getNascimento()));
+
+            preparedStatement.executeUpdate();
+
+            resultSet = preparedStatement.getGeneratedKeys();
+
+            while (resultSet.next()) {
+                paciente.setId(resultSet.getLong(1));
+            }
+
+            System.out.println("Paciente " + paciente.getNome() + " inserido!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            ConnectionManager.fecharConexao(connection, preparedStatement, resultSet);
+        }
+
+        return paciente;
     }
 
     @Override
