b4d212f01ece8221b036717c2115a666e6257a94
diegoreiss <diegoportal.reis@outlook.com>
2022-12-18 17:28:22 -0300

    entity Paciente

---
+++ Consultorio/src/br/com/crud/entities/Paciente.java
@@ -0,0 +1,63 @@
+package br.com.crud.entities;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Paciente {
+    private long id;
+    private String nome;
+    private String cpf;
+    private LocalDate nascimento;
+    private List<Endereco> enderecos;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public LocalDate getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(LocalDate nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    public List<Endereco> getEnderecos() {
+        return enderecos;
+    }
+
+    public void setEnderecos(List<Endereco> enderecos) {
+        this.enderecos = enderecos;
+    }
+
+    @Override
+    public String toString() {
+        return "Paciente{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", nascimento=" + nascimento +
+                ", enderecos=" + enderecos +
+                '}';
+    }
+}
