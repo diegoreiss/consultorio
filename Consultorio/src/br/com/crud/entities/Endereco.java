712e5faf337ea2045ab16a77a25d542e0ce5f394
diegoreiss <diegoportal.reis@outlook.com>
2022-12-18 17:26:46 -0300

    entity Endereco

---
+++ Consultorio/src/br/com/crud/entities/Endereco.java
@@ -0,0 +1,50 @@
+package br.com.crud.entities;
+
+public class Endereco {
+    private long id;
+    private String cep;
+    private String logradouro;
+    private int numero;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    @Override
+    public String toString() {
+        return "Endereco{" +
+                "id=" + id +
+                ", cep='" + cep + '\'' +
+                ", logradouro='" + logradouro + '\'' +
+                ", numero=" + numero +
+                '}';
+    }
+}
