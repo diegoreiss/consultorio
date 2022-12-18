51c9bd1c690bcf2b86c71d15b68f3b26ddccf056
diegoreiss <diegoportal.reis@outlook.com>
2022-12-18 17:49:27 -0300

    classe implementadora da PacienteDAO

---
+++ Consultorio/src/br/com/crud/impl/PacienteDAOImpl.java
@@ -0,0 +1,33 @@
+package br.com.crud.impl;
+
+import br.com.crud.dao.PacienteDAO;
+import br.com.crud.entities.Paciente;
+
+import java.util.List;
+
+public class PacienteDAOImpl implements PacienteDAO {
+    @Override
+    public Paciente criar(Paciente paciente) {
+        return null;
+    }
+
+    @Override
+    public Paciente alterar(Paciente paciente) {
+        return null;
+    }
+
+    @Override
+    public Paciente pesquisarPorId(long id) {
+        return null;
+    }
+
+    @Override
+    public List<Paciente> pesquisarTodos() {
+        return null;
+    }
+
+    @Override
+    public void excluir(long id) {
+
+    }
+}
