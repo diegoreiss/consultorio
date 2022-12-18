911ec711f57dae19f4dde59c5f5a1325f1149d3a
diegoreiss <diegoportal.reis@outlook.com>
2022-12-18 17:49:57 -0300

    classe implementadora da EnderecoDAO

---
+++ Consultorio/src/br/com/crud/impl/EnderecoDAOImpl.java
@@ -0,0 +1,33 @@
+package br.com.crud.impl;
+
+import br.com.crud.dao.EnderecoDAO;
+import br.com.crud.entities.Endereco;
+
+import java.util.List;
+
+public class EnderecoDAOImpl implements EnderecoDAO {
+    @Override
+    public void criar(Endereco endereco, long idPaciente) {
+
+    }
+
+    @Override
+    public Endereco alterar(Endereco endereco) {
+        return null;
+    }
+
+    @Override
+    public Endereco pesquisarPorId(long id) {
+        return null;
+    }
+
+    @Override
+    public List<Endereco> pesquisarTodosPaciente(long idPaciente) {
+        return null;
+    }
+
+    @Override
+    public void excluir(long id) {
+
+    }
+}
